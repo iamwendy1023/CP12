75b4995d9ac6091c5feed1744625fcedb1e48aa5
wendy <iamwendy1023@gmail.com>
2026-02-12 09:19:13 -0800

    Programming in Feb

---
+++ CCC '24 J1 - Conveyor Belt Sushi.py
@@ -0,0 +1,6 @@
+#Sushi problem
+number1 = int(input("number of red plate: ")) #read a line, and will convert it to a number
+number2 = int(input("number of green plate: "))
+number3 = int(input("number of blue plate: "))
+total = number1 * 3 + number2 * 4 + number3 * 5
+print(total)

e6a0505879c1880dae67ea639b062dd9e9c7343d
wendy <iamwendy1023@gmail.com>
2026-02-12 11:59:53 -0800

    Programming in Feb

---
+++ CCC '19 J1 - Winning Score.py
@@ -0,0 +1,15 @@
+#Winning score
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+A = a * 3 + b * 2 + c * 1
+B = d * 3 + e * 2 + f * 1
+if A > B:
+    print("A")
+elif A < B:
+    print("B")
+elif A == B:
+    print("T")

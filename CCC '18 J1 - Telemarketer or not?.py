83fc82f2bca00b31d1790dc9da081dd9d0b2f1df
wendy <iamwendy1023@gmail.com>
2026-02-12 09:53:53 -0800

    Programming in Feb

---
+++ CCC '18 J1 - Telemarketer or not?.py
@@ -0,0 +1,9 @@
+#Telemarketer or not
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+if a in (8, 9) and d in (8, 9) and b == c :
+    print("ignore")
+else:
+    print("answer")

c6cbc7702fed09ed5171b8f9760802bf81739c57
wendy <iamwendy1023@gmail.com>
2026-02-12 09:19:45 -0800

    Programming in Feb

---
+++ CCC '22 J1 - Cupcake Party.py
@@ -0,0 +1,7 @@
+#Cupcake problem
+number4 = int(input())
+number5  = int(input())
+total = number4 * 8 + number5 * 3
+print(total - 28)
+# a variable name can contain letter s and digits and _ only
+# a variable name cannot bein with a digit
